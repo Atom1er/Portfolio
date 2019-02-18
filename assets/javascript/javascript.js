cbd3357610abab0a4a5c11387869839d9a063ce5
Thomas <at93om@gmail.com>
2019-02-18 15:27:12 -0500

    Welcom Section

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,18 +1,12 @@
 
-//Contact form Modal from Bootstrap///
-$('#exampleModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget); // Button that triggered the modal
-    var recipient = button.data('whatever'); // Extract info from data-* attributes
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    var modal = $(this);
-    modal.find('.modal-title').text('New message to ' + recipient);
-    modal.find('.modal-body input').val(recipient);
-  });
-
-
-  /// Animation from ScrollReveal ////
+$(document).ready( function(){
+var center = $('.center');
+var welcome = $('.welcome');
+var MainContent = $('.MainContent');
+MainContent.hide();
+center.on('click', function(){
   window.sr = ScrollReveal();
+
   sr.reveal('#DefaultNav', {
     duration:1500,
     origin: 'top',
@@ -28,25 +22,9 @@ $('#exampleModal').on('show.bs.modal', function (event) {
     origin: 'left',
     distance: '100px'
   });
- 
-  sr.reveal('#SecondRowTitle', {
-    duration:1500,
-    origin: 'left',
-    distance: '100px',
-    viewFactor : 0.2
-  });
-  sr.reveal('#EducationThumbnail', {
-    duration:1000,
-    origin: 'bottom',
-    distance: '200px',
-    viewFactor : 0.2
-  });
-  sr.reveal('#second', {
-    duration:1000,
-    origin: 'bottom',
-    distance: '200px',
-    viewFactor : 0.3
-  });
+  welcome.hide();
+  MainContent.show();
+  
   sr.reveal('#thirdRowTitle', {
     duration:1500,
     origin: 'left',
@@ -65,3 +43,18 @@ $('#exampleModal').on('show.bs.modal', function (event) {
     distance: '200px',
     viewFactor : 0.3
   });
+});
+
+
+
+//Contact form Modal from Bootstrap///
+$('#modal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var recipient = button.data('whatever'); // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this);
+    modal.find('.modal-title').text('New message to ' + recipient);
+    modal.find('.modal-body input').val(recipient);
+  });
+});
